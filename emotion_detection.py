98a64aab0cdaa99608db3ccbc56f59164d6631a2
urvashia2594 <dhakeurvashi7@gmail.com>
2025-06-26 03:48:54 -0400

    Intial commit up to step 3 done formatting response

---
+++ emotion_detection.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyze } }
+
+    response =  requests.post(URL, json = myobj, headers = header)
+
+    formmated_text = json.loads(response.text)
+
+    emotions = formmated_text['emotionPredictions'][0]['emotion']
+
+    dominant_emotion = max(emotions, key = emotions.get)
+
+    emotions['dominant_emotion'] = dominant_emotion
+
+    return emotions
+
